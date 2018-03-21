6af3833e5f7a5d4da290b65559b409ee0b682b9a
Garnett Puett and Kelsey Guerra <kelsey@>
2018-03-21 09:21:00 -0700

    Made postcard.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    var nameInput = $("input#name").val();
+
+    $(".name").text(nameInput)
+    $("#letter").show();
+
+    event.preventDefault();
+  })
+})
